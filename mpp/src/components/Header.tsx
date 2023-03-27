7b7009c84ee35701478b99c299707fde6f026c0f
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-03-27 22:55:46 +0300

    implemented the header

--- mpp/src/components/Header.tsx
+++ mpp/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import './Header.scss'
 
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
 const Header = () => {
 
     return (
         <div className="header">
-
+            <DashboardIcon sx={{ fontSize: 40 }} className='header-icon'/>
+            <p className='header-title'>Admin Dashboard</p>
         </div>
     );
 }
--- mpp/src/components/Teste.tsx
+++ mpp/src/components/Teste.tsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DealershipRequests from "../api/DealershipRequests";
 
 const Teste = () => {
 
-    const [text, setText] = useState<string>();
+    const [text, setText] = useState<string>("");
 
-    DealershipRequests.getDealerships().then(res => {
-        console.log(res.data);
-        setText(res.data);
-    });
+    useEffect(() => {
+        const fetchDealerships = async () => {
+            try {
+                const response = await DealershipRequests.getDealerships();
+                setText(JSON.stringify(response.data));
+            } catch (err: any) {
+                if (err.response) {
+                    console.log("Error fetching dealerships");
+                    console.log(err.response.data.message);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log("Error: " + err.message);
+                }
+            }
+        }
+
+        fetchDealerships();
+    }, []);
 
     return (
         <p>
