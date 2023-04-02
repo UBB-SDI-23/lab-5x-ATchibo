65864425542b8b0a91c56606771b0b1308c91575
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-04-02 17:29:18 +0300

    added aws ip as base backend ip

--- src/Values.tsx
+++ src/Values.tsx
@@ -10,7 +10,8 @@ class Values {
     static secondaryColor = "#0c5937";
 
     // Backend
-    static baseBackendUrl = "http://localhost:8080";
+    // static baseBackendUrl = "http://localhost:8080";
+    static baseBackendUrl = "http://13.51.144.35";
     
     // Dealerships
     static dealershipsBaseUrl = "/dealerships";
