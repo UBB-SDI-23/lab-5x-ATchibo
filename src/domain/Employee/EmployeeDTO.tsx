c40c057b2274e4531dc24ed7f3e282c200488a66
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-04-20 16:20:24 +0300

    created employee info and dto

---
+++ src/domain/Employee/EmployeeDTO.tsx
@@ -0,0 +1,127 @@
+// import { IsEmail, IsNotEmpty, Min } from 'class-validator';
+
+export class EmployeeDTO {
+    private id: number;
+    private name: string;
+    private role: string;
+    private email: string;
+    private phone: string;
+    private salary: number;
+    private dealershipId: number;
+    private dealershipName: string;
+
+    constructor(
+        idOrDTO?: number | EmployeeDTO | any,
+        name?: string,
+        role?: string,
+        email?: string,
+        phone?: string,
+        salary?: number,
+        dealershipId?: number,
+        dealershipName?: string
+    ) {
+        if (typeof idOrDTO === 'number') {
+            this.id = idOrDTO;
+            this.name = name || '';
+            this.role = role || '';
+            this.email = email || '';
+            this.phone = phone || '';
+            this.salary = salary || 0;
+            this.dealershipId = dealershipId || -1;
+            this.dealershipName = dealershipName || '';
+        } else if (idOrDTO instanceof EmployeeDTO) {
+            const dto = idOrDTO;
+            this.id = dto.id;
+            this.name = dto.name;
+            this.role = dto.role;
+            this.email = dto.email;
+            this.phone = dto.phone;
+            this.salary = dto.salary;
+            this.dealershipId = dto.dealershipId;
+            this.dealershipName = dto.dealershipName;
+        } else if (typeof idOrDTO === 'object') {
+            const dto = idOrDTO as any;
+            this.id = dto.id || 0;
+            this.name = dto.name || '';
+            this.role = dto.role || '';
+            this.email = dto.email || '';
+            this.phone = dto.phone || '';
+            this.salary = dto.salary || 0;
+            this.dealershipId = dto.dealershipId || -1;
+            this.dealershipName = dto.dealershipName || '';
+        } else {
+            this.id = 0;
+            this.name = '';
+            this.role = '';
+            this.email = '';
+            this.phone = '';
+            this.salary = 0;
+            this.dealershipId = -1;
+            this.dealershipName = '';
+        }
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    setId(id: number): void {
+        this.id = id;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    setName(name: string): void {
+        this.name = name;
+    }
+
+    getRole(): string {
+        return this.role;
+    }
+
+    setRole(role: string): void {
+        this.role = role;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    setEmail(email: string): void {
+        this.email = email;
+    }
+
+    getPhone(): string {
+        return this.phone;
+    }
+
+    setPhone(phone: string): void {
+        this.phone = phone;
+    }
+
+    getSalary(): number {
+        return this.salary;
+    }
+
+    setSalary(salary: number): void {
+        this.salary = salary;
+    }
+
+    getDealershipId(): number {
+        return this.dealershipId;
+    }
+
+    setDealershipId(dealershipId: number): void {
+        this.dealershipId = dealershipId;
+    }
+
+    getDealershipName(): string {
+        return this.dealershipName;
+    }
+
+    setDealershipName(dealershipName: string): void {
+        this.dealershipName = dealershipName;
+    }
+}
---
+++ src/domain/Employee/EmployeeInfo.tsx
@@ -0,0 +1,46 @@
+import { GridColDef } from "@mui/x-data-grid";
+import { EmployeeDTO } from "./EmployeeDTO";
+
+
+class EmployeeInfo {
+    static columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'name', headerName: 'Name', width: 200 },
+        { field: 'role', headerName: 'Role', width: 230},
+        { field: 'email', headerName: 'Email', width: 150 },
+        { field: 'phone', headerName: 'Phone', width: 130 },
+        { field: 'salary', headerName: 'Salary', width: 130 },
+        { field: 'dealershipName', headerName: 'Dealership Name', width: 200}
+    ];
+
+    static isNameValid = (c: EmployeeDTO) => {
+        return c.getName().length > 0;
+    }
+
+    static isRoleValid = (c: EmployeeDTO) => {
+        return c.getRole().length > 0;
+    }
+
+    static isEmailValid = (c: EmployeeDTO) => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(c.getEmail()).toLowerCase());
+    }
+
+    static isPhoneValid = (c: EmployeeDTO) => {
+        return c.getPhone().length > 0;
+    }
+
+    static isSalaryValid = (c: EmployeeDTO) => {
+        return c.getSalary() >= 100;
+    }
+
+    static isDealershipValid = (c: EmployeeDTO) => {
+        return c.getDealershipId() > 0;
+    }
+
+    static isValid = (c: EmployeeDTO) => {
+        return this.isNameValid(c) && this.isRoleValid(c) && this.isEmailValid(c) && this.isPhoneValid(c) && this.isSalaryValid(c) && this.isDealershipValid(c);
+    }
+} 
+
+export default EmployeeInfo;
