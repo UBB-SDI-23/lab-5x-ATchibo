60896ba52dc6e58272203b1a25940bd02ec486a8
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-05-08 16:37:46 +0300

    changed backend

--- src/Values.tsx
+++ src/Values.tsx
@@ -17,8 +17,8 @@ class Values {
     static secondaryColor = "#0c5937";
 
     // Backend
-    static baseBackendUrl = "http://localhost:8080/api";
-    // static baseBackendUrl = "https://exquisite-fox.chickenkiller.com/api";
+    // static baseBackendUrl = "http://localhost:8080/api";
+    static baseBackendUrl = "https://exquisite-fox.chickenkiller.com/api";
     
     // Dealerships
     static dealershipsBaseUrl = "/dealerships";
