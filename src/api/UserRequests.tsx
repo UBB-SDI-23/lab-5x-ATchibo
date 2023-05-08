eb5162fb42d4a005fd36426fd22d750abdc36e4a
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-05-08 14:49:54 +0300

    cleanup

--- src/api/UserRequests.tsx
+++ src/api/UserRequests.tsx
@@ -4,13 +4,7 @@ import { getApi } from "../helpers/AxiosHelper";
 class UserRequests {
 
     async getAllUsers(page: number = 0, size: number = 0): Promise<any> {
-
         return await getApi(Values.baseBackendUrl).get(Values.usersBaseUrl + Values.usersAllUrl + "?page=" + page.toString() + "&size=" + size.toString());
-        // return await request(
-        //     "GET",
-        //     Values.usersBaseUrl + Values.usersAllUrl + "?page=" + page.toString() + "&size=" + size.toString(),
-        //     {}
-        // )
     }
 
     async getAllUsersJson(page: number = 0, size: number = 0): Promise<JSON[]> {
@@ -19,23 +13,11 @@ class UserRequests {
     }
 
     async getCurrentUser(): Promise<any> {
-
         return await getApi(Values.baseBackendUrl).get(Values.usersBaseUrl + Values.usersCurrentUrl);
-        // return await request(
-        //     "GET",
-        //     Values.usersBaseUrl + Values.usersCurrentUrl,
-        //     {}
-        // )
     }
 
     async getUser(username: string): Promise<any> {
-
         return await getApi(Values.baseBackendUrl).get(Values.usersBaseUrl + Values.usersByUsernameUrl + "/"  + username);
-        // return await request(
-        //     "GET",
-        //     Values.usersBaseUrl + Values.usersByUsernameUrl + "/"  + username,
-        //     {username: username}
-        // )
     }
 
     async getUserJson(username: string): Promise<JSON> {
@@ -44,33 +26,15 @@ class UserRequests {
     }
 
     async getUserNrDealerships(userId: number): Promise<any> {
-
         return await getApi(Values.baseBackendUrl).get(Values.usersBaseUrl + "/"  + userId.toString() + Values.usersCountNrDealershipsUrl);
-        // return await request(
-        //     "GET",
-        //     Values.usersBaseUrl + "/"  + userId.toString() + Values.usersCountNrDealershipsUrl,
-        //     {id: userId}
-        // )
     }
 
     async getUserNrEntitiesAdded(userId: number): Promise<any> {
-
         return await getApi(Values.baseBackendUrl).get(Values.usersBaseUrl + "/"  + userId.toString() + Values.usersCountNrEntitiesAddedUrl);
-        // return await request(
-        //     "GET",
-        //     Values.usersBaseUrl + "/"  + userId.toString() + Values.usersCountNrEntitiesAddedUrl,
-        //     {id: userId}
-        // )
     }
 
     async updateUserRole(userId: number, role: string): Promise<any> {
-
         return await getApi(Values.baseBackendUrl).put(Values.usersBaseUrl + "/" + userId.toString() + Values.usersUpdateRoleUrl, {role});
-        // return await request(
-        //     "PUT",
-        //     Values.usersBaseUrl + "/" + userId.toString() + Values.usersUpdateRoleUrl,
-        //     {role}
-        // )
     }
 
     async insertBulkData(): Promise<any> {
@@ -78,9 +42,6 @@ class UserRequests {
     }
 
     async setPageSize(size: number): Promise<any> {
-        console.log("setPageSize");
-        console.log(size);
-        console.log(Values.usersBaseUrl + Values.usersSetPageSizeUrl + "/" + size);
         return await getApi(Values.baseBackendUrl).post(Values.usersBaseUrl + Values.usersSetPageSizeUrl + "/" + size);
     }
 }
